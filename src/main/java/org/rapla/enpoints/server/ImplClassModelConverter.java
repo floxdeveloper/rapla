2efe5eef7f368ce17efc2b1c74b0ebe07eb7d70a
floxdeveloper <fritz66@alpenjodel.de>
2026-04-09 18:05:00 +0200

    Add further combinations of Interface Impl to entries for schema conversion

--- src/main/java/org/rapla/enpoints/server/ImplClassModelConverter.java
+++ src/main/java/org/rapla/enpoints/server/ImplClassModelConverter.java
@@ -14,12 +14,74 @@ import com.fasterxml.jackson.databind.JavaType;
 public class ImplClassModelConverter implements ModelConverter {
 
     private static final Map<Class<?>, Class<?>> INTERFACE_TO_IMPL = Map.ofEntries(
+            // org.rapla.entities.dynamictype
             Map.entry(
                     org.rapla.entities.dynamictype.Classification.class,
                     org.rapla.entities.dynamictype.internal.ClassificationImpl.class),
             Map.entry(
+                    org.rapla.entities.dynamictype.Attribute.class,
+                    org.rapla.entities.dynamictype.internal.AttributeImpl.class),
+            Map.entry(
+                    org.rapla.entities.dynamictype.ClassificationFilter.class,
+                    org.rapla.entities.dynamictype.internal.ClassificationFilterImpl.class),
+            Map.entry(
+                    org.rapla.entities.dynamictype.ClassificationFilterRule.class,
+                    org.rapla.entities.dynamictype.internal.ClassificationFilterRuleImpl.class),
+            Map.entry(
+                    org.rapla.entities.dynamictype.DynamicType.class,
+                    org.rapla.entities.dynamictype.internal.DynamicTypeImpl.class),
+            
+            // org.rapla.entities.domain
+            Map.entry(
                     org.rapla.entities.domain.Allocatable.class,
-                    org.rapla.entities.domain.internal.AllocatableImpl.class)
+                    org.rapla.entities.domain.internal.AllocatableImpl.class),
+            Map.entry(
+                    org.rapla.entities.domain.Reservation.class,
+                    org.rapla.entities.domain.internal.ReservationImpl.class),
+            Map.entry(
+                    org.rapla.entities.domain.Appointment.class,
+                    org.rapla.entities.domain.internal.AppointmentImpl.class),
+            Map.entry(
+                    org.rapla.entities.domain.Permission.class,
+                    org.rapla.entities.domain.internal.PermissionImpl.class),
+            Map.entry(
+                    org.rapla.entities.domain.Period.class,
+                    org.rapla.entities.domain.internal.PeriodImpl.class),
+            
+            // org.rapla.entities (root level)
+            Map.entry(
+                    org.rapla.entities.Category.class,
+                    org.rapla.entities.internal.CategoryImpl.class),
+            Map.entry(
+                    org.rapla.entities.User.class,
+                    org.rapla.entities.internal.UserImpl.class),
+            
+            // org.rapla.entities.configuration
+            Map.entry(
+                    org.rapla.entities.configuration.CalendarModelConfiguration.class,
+                    org.rapla.entities.configuration.internal.CalendarModelConfigurationImpl.class),
+            Map.entry(
+                    org.rapla.entities.configuration.Preferences.class,
+                    org.rapla.entities.configuration.internal.PreferencesImpl.class),
+            Map.entry(
+                    org.rapla.entities.configuration.RaplaMap.class,
+                    org.rapla.entities.configuration.internal.RaplaMapImpl.class),
+            
+            // org.rapla.entities.storage
+            Map.entry(
+                    org.rapla.entities.storage.ExternalSyncEntity.class,
+                    org.rapla.entities.storage.internal.ExternalSyncEntityImpl.class),
+            
+            // org.rapla.facade
+            Map.entry(
+                    org.rapla.facade.CalendarOptions.class,
+                    org.rapla.facade.internal.CalendarOptionsImpl.class),
+            Map.entry(
+                    org.rapla.facade.CalendarSelectionModel.class,
+                    org.rapla.facade.internal.CalendarModelImpl.class),
+            Map.entry(
+                    org.rapla.facade.Conflict.class,
+                    org.rapla.facade.internal.ConflictImpl.class)
     );
 
     @Override
